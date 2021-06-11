4ca1ffa7e3cd72dbd7a10567c9349a20d03e3f41
furqan4545 <furqan.ali9500@gmail.com>
2021-06-11 19:26:00 +0900

    sadasdd

--- apis.js
+++ apis.js
@@ -455,7 +455,7 @@ app.get("/check_me/:uname", async (req, res) => {
 
         console.log("This is query 1 result : ",  values);
         // var result = query2.rows[0];
-        console.log("HIIII: ", query.rows[0]);
+        console.log("HIIII: ", query.rows[0].patient_key);
         // res.json(query2.rows);
         // if (res.json(query2.rows) != null){
         //     res.json("Interation found");
